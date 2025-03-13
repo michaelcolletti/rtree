d30fe02f178874bdf279b575e5d72ef9c673a360
Michael <michael@cpossibilities.com>
2025-03-13 13:41:44 -0400

    Added tests for main.rs

---
+++ tests/test_main.rs
@@ -0,0 +1,9 @@
+// tests/test_main.rs
+use rstest::rstest;
+
+#[rstest]
+#[case(1, 2, 3)]
+#[case(2, 3, 5)]
+fn test_add(#[case] a: i32, #[case] b: i32, #[case] expected: i32) {
+    assert_eq!(a + b, expected);
+}
